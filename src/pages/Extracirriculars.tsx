f8bffca8c05fb238e2142ab1db37b20541568fbd
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-11-01 23:26:38 -0400

    Populated extracirriculars page

--- src/pages/Extracirriculars.tsx
+++ src/pages/Extracirriculars.tsx
@@ -1,52 +1,67 @@
 //import './css/Projects.css'
+import lacrosse from '../assets/lacrosse.jpeg'
+import ccpm from '../assets/CCMP.jpeg'
+import atc from '../assets/ATC.jpeg'
+
 
 function Extracirriculars() {
     return (
     <div className="projects-page">
         <div className="project-container">
             <div className='proj-pic'>
-                
+                <img src={lacrosse} alt="Image Failed to Load"></img>
             </div>
             <div className="description">
                 <h2>
-                    In-Situ Environmental Sensor Collection and Analytics Platform
+                    Offensive Coordinator UGA Men's Club Lacrosse
                 </h2>
-                <h4>
-                    Description
-                </h4>
                 <p>
-                    The SitS system is an NSF-funded project aimed at creating a remote 
-                    sensing platform that is able to collect and store environmental data 
-                    without the need for outside power or network connections. The system 
-                    would automate data collection, allowing for cheap and reliable access
-                    to a large amount of environmental data like soil composition.
+                    Serve as the current offensive coordinator, leading team strategy and setting team goals. 
                 </p>
-                <h4>
-                    Purpose
-                </h4>
                 <p>
-                    This project aims to create a reliable and low-cost data collection 
-                    method that requires limited researcher interaction. Currently, 
-                    researchers have to manually take soil core samples to retreive
-                    the data we are targeting, and this is ineffective for tracking 
-                    long-term changes over time. We seek to create a large data set 
-                     tracking changes in soil organic matter content in order to yeild 
-                     a spacially and temporally significant data set that can be used
-                     to determine the health of the environment.
-                     
-                </p>
-                <h4>
-                    Progress
-                </h4>
-                <p>Utilizing the FabLab at UGA we constructed the platform
-                    that will be deployed in the saltwater marshland. The 
-                    next steps will be testing the platform at a location in 
-                    Athens, GA to ensure proper sensing operations and platform
-                    stability. 
+                    Former Event Coordinator and Captain (2022-2023) where I led team events, aquired team sponsorships, and 
+                    independently managed a travel budget exceeding $35,000 and organized cross-country team travel. In Spring of
+                    2023 after a 13-1 season we successfully made it to the second round of the final tournament in Round Rock, TX, 
+                    having not been to the tournament in over 10 years. 
                 </p>
             </div>
         </div>
-    </div>
+        <div className="project-container">
+            <div className='proj-pic'>
+                <img src={ccpm} alt="Image Failed to Load"></img>
+            </div>
+            <div className="description">
+                <h2>
+                    Student Mentor: Clarke County Mentor Program
+                </h2>
+                <p>
+                    Clarke County Mentor Program connects mentors with students within Athens-Clarke county to provide
+                    mentorship advice and tutoring to students seeking guidance.
+                    <a href="https://www.clarkecountymentorprogram.org/"></a>
+                </p>
+                <p>
+                    I've been a part of the program since my Freshman year at UGA, seeking to inspire my mentees to 
+                    dream big and work hard to achieve their goals. For more information visit their website: 
+                    <a href="https://www.clarkecountymentorprogram.org/">Clarke Country Mentor Program</a>
+                </p>
+            </div>
+        </div>
+        <div className="project-container">
+            <div className='proj-pic'>
+                    <img src={atc} alt="Image Failed to Load"></img>
+            </div>
+                <div className="description">
+                    <h2>
+                        Student Volunteer: Athens Technical College ESL Courses
+                    </h2>
+                    <p>
+                        Athens Technical College offers English as a Second Language (ESL) courses teaching English to non-native 
+                        speakers. I had the opportunity to volunteer assisting to teach Spanish speaking students English at a variety 
+                        of levels.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
