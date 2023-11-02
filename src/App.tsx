af8603f7ea127972df82463235779b31ab8f421d
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-11-02 09:44:43 -0400

    removed changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,19 @@ import Resume from './components/Resume'
 import SpotifyApp from './pages/SpotifyApp'
 import Extracirriculars from './pages/Extracirriculars'
 import { Route, Routes } from "react-router-dom"
-import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
     <>
         <NavBar />
         <div className="container">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/resume-website" element={<Home />} />
-              <Route path="/resume-website/projects" element={<Projects />} />
-              <Route path="/resume-website/resume" element={<Resume />} />
-              <Route path="/resume-website/spotify" element={<SpotifyApp />} />
-              <Route path="/resume-website/extracirriculars" element={<Extracirriculars />} />
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route path="/resume-website" element={<Home />} />
+            <Route path="/resume-website/projects" element={<Projects />} />
+            <Route path="/resume-website/resume" element={<Resume />} />
+            <Route path="/resume-website/spotify" element={<SpotifyApp />} />
+            <Route path="/resume-website/extracirriculars" element={<Extracirriculars />} />
+          </Routes>
         </div>
       </>
   )
